f117c151a4913ca4f5614d2d481b9a4e9cee9cb4
99persent <99persent@DESKTOP-O387PA2>
2024-01-04 17:18:34 +0530

    new one add

--- dynamicproject/src/main/java/dynamicproject/first.java
+++ dynamicproject/src/main/java/dynamicproject/first.java
@@ -4,7 +4,7 @@ public class first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello java");
+		System.out.println("hello  java program");
 	}
 
 }
